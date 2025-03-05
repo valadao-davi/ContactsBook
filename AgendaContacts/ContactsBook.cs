c83561f69fdfe7a2928b6651df290e3d7eecf8f6
valadao-davi <davi.raichu@gmail.com>
2025-03-05 17:55:34 -0300

    feat: contacts book class and addcontact method

---
+++ AgendaContacts/ContactsBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContacts
+{
+    class ContactsBook
+    {
+        private Dictionary<string, Contact> contactsBook = new Dictionary<string, Contact>;
+    }
+}
